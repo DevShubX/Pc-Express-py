d61ff08b585851d55bd10ccbb33a8410fc5515b8
Shubham-destro <60731101+Shubham-destro@users.noreply.github.com>
2020-11-15 13:20:56 +0530

    Update Source code.py

--- PC Express/Source code.py
+++ PC Express/Source code.py
@@ -1,73 +1,127 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="")
-mycursor=mydb.cursor()
-mycursor.execute("Create Database PC_Express")
+print("--------------------              PC EXPRESS            ---------------------")
+print('\n')
+print("--------------------             DATABASE 2020          ---------------------")
+print("\n")
 
-# creating tables for project
+def menu():
+    c='y'
+    while(c=='y'):
+        print('1.PC Component')
+        print('2.Sales Detail')
+        print('3.Exit')
+        print('\n')
+        choice=int(input('Enter the number:'))
+        print('\n')
+        if choice==1:
+            print('1.Motherboard')
+            print('2.Processor')
+            print('3.Ram')
+            print('4.Storage')
+            print('5.Graphic card')
+            print('6.Power Supply')
+            print('7.Cabinet')
+            print('8.EXIT')
+            print('\n')
+            a=int(input('Enter the number:'))
+            print('\n')
+            if a==1:
+                print('1.Add Motherboard Record')
+                print('2.Update Motherboard Record')
+                print('3.Display Motherboard Record')
+                print('4.Modify Motherboard Record')
+                print('5.Delete Record from table')
+                print('6.Main Menu')
+                print('\n')
+                x=int(input('Enter the number'))
+                if x==1:
+                    addmb()
+                elif x==2:
+                    upmb()
+                elif x==3:
+                    dismb()
+                elif x==4:
+                    modmb()
+                elif x==5:
+                    delmb()
+                else :
+                    print('Returning to main menu')
+def addmb():
+    import mysql.connector
+    mydb=mysql.connector.connect(host='localhost',user='root',passwd="",db="pc_express")
+    mycursor=mydb.cursor()
+    name=input('Model Name:')
+    company=input('Company Name:')
+    soctype=input("socket_type Name:")
+    chip=input('Chipset Name:')
+    qty=input('QUANTITY:')
+    price=input('Enter Price:')
+    mycursor.execute("""INSERT INTO motherboard(Name,Company,Socket_type,Chipset,Qty,Price)VALUES(%s,%s,%s,%s,%s,%s)""",(name,company,soctype,chip,qty,price))
+    mydb.commit()
+    print("Done")
 
-# table 1 for motherboards in stock and price
+def upmb():
+    import mysql.connector
+    try:
+        mydb=mysql.connector.connect(host='localhost',user='root',passwd="",db="pc_express")
+        mycursor=mydb.cursor()
+        U=input("Enter the SQL Command for update:")
+        mycursor.execute(U)
+        mydb.commit()
+        print('Record Updated')
 
-import mysql.connector
+    except Exception as e:
+        print(e)
 
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Motherboard(Name varchar(100),Company varchar(50),Socket_type varchar(50),Chipset varchar(50),Qty char(100),Price char(100))")
+def dismb():
+    import mysql.connector
+    try:
+        mydb=mysql.connector.connect(host='localhost',user='root',passwd="",db="pc_express")
+        mycursor=mydb.cursor()
+        D=input("enter the statement to Fetch from table ")
+        mycursor.execute(D)
+        print("Data Displayed")
+        mydb.commit()
+    except:
+        print("Error:unable to fetch data.Please try again")
+def modmb():
+    import mysql.connector
+    try:
+        mydb=mysql.connector.connect(host='localhost',user='root',passwd="",db="pc_express")
+        mycursor=mydb.cursor()
+        w=input("Enter the Sql Command:")
+        mycursor.execute(w)
+        mydb.commit()
+        print("Table is Modified")
+    except Exception as e:
+        print(e)
+        print("Unable to Modify")
+
+def delmb():
+    import mysql.connector
+    try:
+        mydb=mysql.connector.connect(host='localhost',user='root',passwd="",db="pc_express")
+        mycursor=mydb.cursor()
+        r=input("Enter The sql command:")
+        mycursor.execute(r)
+        mydb.commit()
+        print("Record Deleted")
+    except Exception as e:
+        print(e)
+        print("Unable to delete")
+
+menu()
 
 
-# table 2 for processor
+        
 
-import mysql.connector
 
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Processor(Company varchar(100),Name varchar(50),Cores int(4),Socket_type varchar(100),Base_clock varchar(100),Boost_clock varchar(100),Qty char(100),Price int(100))")
 
-# table 3 for Ram
 
-import mysql.connector
 
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Ram(Company varchar(100),Name varchar(50),Size varchar(5),Speed varchar(100),Ram_type varchar(100),Qty varchar(100),Price varchar(100))")
 
-# table 4 for storage
 
-import mysql.connector
 
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Storage(Company varchar(100),Name varchar(50),Type varchar(100),Speed varchar(100),Qty varchar(100),Price varchar(100))")
 
-# table 5 for GPU
 
-import mysql.connector
-
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Graphic_Cards(Company varchar(100),Name varchar(100),Memory_type varchar(100),Vram varchar(100),Qty varchar(100),Price varchar(100))")
-
-# table 6 for power supply
-
-import mysql.connector
-
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Power_supply(Company varchar(100),Name varchar(100),Type varchar(100),Watts varchar(100),Qty varchar(100),Price varchar(100))")
-
-# table 7 for cabinet
-
-import mysql.connector
-
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Cabinet(Company varchar(100),Name varchar(100),Form_Factor varchar(100),Qty varchar(100),Price varchar(100))")
-
-# table 8 for Sales and customer details
-
-import mysql.connector
-
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="",db="pc_express")
-mycursor=mydb.cursor()
-mycursor.execute("create table Sales_Details(C_name varchar(100),Phone_NO varchar(100),Description_of_Goods varchar(100),Date_of_purchase date,Qty varchar(100),Price varchar(100))")
 
 
